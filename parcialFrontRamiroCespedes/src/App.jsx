f3c121194fa1ea616d68e355e3532d2bd7b858f1
ramirocespedes <ramirocm2019@gmail.com>
2023-08-14 20:49:10 -0300

    Primer Commit

---
+++ parcialFrontRamiroCespedes/src/App.jsx
@@ -0,0 +1,18 @@
+import Card from './Components/Card.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ parcialFrontRamiroCespedes/src/Components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>esto es una card</div>
+  )
+}
+
+export default Card
